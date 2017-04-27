0ac527755efe00bd4151e3a5dd6dfe423138080a
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2017-04-27 10:41:33 -0400

    Improved comments.

--- main/src/ca/uwaterloo/flix/language/phase/Implicits.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Implicits.scala
@@ -24,39 +24,28 @@ import ca.uwaterloo.flix.util.Validation
 import ca.uwaterloo.flix.util.Validation._
 import ca.uwaterloo.flix.util.collection.MultiMap
 
-// TODO: Do we want to allow explicit implicit parameters, e.g.:
 //
-// LocalVar(r, sum(?ctx, ?stm, v1, v2)) :- AddStm(r, x, y), LocalVar(x, v1), LocalVar(y, v2).
+// Open Questions:
 //
-// We can, but that is an extension.
+// 1. Do we want to allow explicit implicit parameters, e.g. ?width, ?ctx, ?stm, etc. I think we do.
 //
+// 2. We have to ensure safety, i.e. no unbound variables in head predicates, e.g.:
+//    We have to disallow: P(implicit$1). and P(implicit$1) :- A(x). etc.
+//
+// 3. Introduce syntax for putting an explicit parameter into implicit scope (i.e. allowing an explicit parameter to unify with an implicit).
+//
+// 4. What does it mean for an implicit to be ambiguous here (and in Scala)?
+//
+// 5. Is it okay to have an implicit parameter alone?
 
-// TODO: We have to be careful with implicit variables not bound in the body. For example, this rule is unsafe:
-//
-// R(x, ?y) :- A(x) // where A does not define an implicit y.
-//
-// This goes back to the notion of safe rules.
-//
-// Essentially we want implicits to have 2-3 properties:
-//
-// 1. Preserves type safety.
-// 2. preservers rule safety. (no unbound head variables).
-// 3. Unambigious / deterministic.
-// 4. (Optional) disallow a single occurence of an implicit parameter? (Perhaps not, after all we can use relevance types for that).
-//
-// In Scala an implicit can be ambiguios, what does it mean here?
-//
 
-// TODO: Introduce an equivalance abstraction which allows *n* implicits to be equivalent with at most one explicitCanUnify.
-
-/**
-  * TODO: Important idea: Allow explicit parameters to be annoted with ! to
-  * indicate that hthey may be unified with implicit variables. E.g.
-  *
-  * VarPointsToIn(!s2) :−
-  * CFG(!s1, !s2),
-  * VarPointsToOut(!s1).
-  */
+// Properties:
+//
+// We want to prove these properties for our implicits:
+//
+//    1. Type safety.
+//    2. Rule safety (no unbound head variables).
+//    3. Unique translation.
 
 /**
   * Computes equivalences of implicit parameters in constraints.
@@ -81,9 +70,6 @@ object Implicits extends Phase[TypedAst.Root, TypedAst.Root] {
     * Performs implicit resolution on the given constraint `s`.
     */
   def implicify(c: TypedAst.Constraint): TypedAst.Constraint = {
-    // A map from types to symbols.
-    val type2sym = getType2Symbols(c)
-
     // An equivalence relation on implicit variable symbols that share the same type.
     val m = new MultiMap[Symbol.VarSym, Symbol.VarSym]
 
@@ -117,11 +103,11 @@ object Implicits extends Phase[TypedAst.Root, TypedAst.Root] {
 
     // TODO: Handle conflicts.
 
+    // Compute equivalences of implicit variables not unified with an explicit variable.
     val byType = new MultiMap[Type, Symbol.VarSym]
 
     // Iterate through all implicits variable and unify those that do not yet belong to an equivalence class.
     for ((implicitSym, implicitType) <- implicitParamsOf(c)) {
-
       // Check that the implicit sym is not already used.
       if (!m.keys.contains(implicitSym) && !m.values.exists(_.contains(implicitSym))) {
         byType.put(implicitType, implicitSym)
@@ -172,17 +158,6 @@ object Implicits extends Phase[TypedAst.Root, TypedAst.Root] {
     }
     case TypedAst.Predicate.Body.Negative(sym, terms, loc) => false // TODO: Is it right to return false here?
     case TypedAst.Predicate.Body.NegativeOverloaded(_, implicits, loc) => ??? // TODO
-  }
-
-  /**
-    * Returns a map from types to variable symbols for the given constraint `c`.
-    */
-  def getType2Symbols(c: TypedAst.Constraint): MultiMap[Type, Symbol.VarSym] = {
-    val m = new MultiMap[Type, Symbol.VarSym]
-    for (cparam <- c.cparams) {
-      m.put(cparam.tpe, cparam.sym)
-    }
-    m
   }
 
   /**
